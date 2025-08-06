4e03d2d05d392ee7f1c422f60309cb782fede4a2
Prachi Sharma <145744960+prachisharma1277@users.noreply.github.com>
2025-08-06 22:48:21 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -6,5 +6,5 @@ class Config:
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///mydb.sqlite3'
+    SQLALCHEMY_DATABASE_URI =os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
