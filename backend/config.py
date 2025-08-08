d528216850fcce6635fbeaae5bff960cda8b0fb1
Prachi Sharma <145744960+prachisharma1277@users.noreply.github.com>
2025-08-08 08:17:51 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -6,5 +6,5 @@ class Config:
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI =os.getenv("DATABASE_URL") or "postgresql://postgres:b6!s<HS/agamy123@db.srulgrsvkaeabkvywgiz.supabase.co:5432/postgres"
+    SQLALCHEMY_DATABASE_URI =os.getenv("DATABASE_URL") or "postgresql://postgres:b6%21s%3CHS%2Fagamy123@db.srulgrsvkaeabkvywgiz.supabase.co:5432/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
